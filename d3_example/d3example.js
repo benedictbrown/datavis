b41c0e68ad9ee4f73b84b85308825cfeb340144f
Benedict J. Brown <github@bjbrown.us>
2020-09-09 11:43:14 -0400

    finished example

--- d3_example/d3example.js
+++ d3_example/d3example.js
@@ -1,26 +1,33 @@
-let sampleData1 = [ 3, 1, 4, 1, 5, 9 ];
-let sampleData2 = [ 2, 7, 1, 8, 2, 8 ];
 
 const padding = 50; // 20 pixels of padding around the plot
+let index = 0;    // we have a 2d array of numbers; this says which row to plot
+let sampleData = []; // place holder for the sample data that will be loaded next
 
+// load and parse d3example.csv
+// web browsers will only load external files over the network, not local files, so you have to start up a web server
+// the simplest way to do this is to run "python -m http.server 8888" in this directory
+// if you are using python 2, replace http.server with SimpleHTTPServer
+
+// d3.csv will load a csv directly, but it assumes you have a header row with column labels
+// we don't have that, so we have to load it as a text file, then use csvParseRows to
+// parse the data, then process it
+
+// The syntax with .then(...) is different than what is in the Murray text
+// this is called a javascript "promise" and is part of ES6.  d3 adopted
+// it in version 5 (the book uses version 4).  It has the advantages
+// of chaining callbacks rather than nesting them (easier to read)
+// and letting you use the await keyword.  We don't use the await
+// keyword here because it only works inside functions that are declared async.
+d3.text('d3example.csv') // load d3example.csv as a text file
+  .then((data) => d3.csvParseRows(data, d3.autoType)) // parse the CSV data into a 2D array, without assuming a header row
+  .then((data) => {                                   // d3.autoType is new in d3 v5, and converts numbers instead of leaving them strings
+    sampleData = data;
+    updatePlot();
+  });
+
+// swap which row of the CSV file we show
 function toggleData() {
-  /*
-  console.log('toggleData');
-  const tmp = [ ...sampleData1 ]; // make copy of data1 values
-  console.log(JSON.stringify(tmp));
-  console.log(JSON.stringify(sampleData1));
-  console.log(JSON.stringify(sampleData2));
-  for (let i = 0; i < sampleData1.length; i++) {
-    sampleData1[i] = sampleData2[i];
-    sampleData2[i] = tmp[i];
-  }
-  console.log(JSON.stringify(sampleData1));
-  console.log(JSON.stringify(sampleData2));
-  */
-  const tmp = sampleData1;
-  sampleData1 = sampleData2;
-  sampleData2 = tmp;
-
+  index = 1 - index; // toggle which row of data we are plotting
   updatePlot();
 }
 
@@ -36,16 +43,16 @@ function updatePlot() {
   // also go to 1 less than the minimum value and 1 more than the max in each direction
 
   // create an x scale that maps the width of the SVG to the number of elements we have, with padding
-  const xScale = d3.scaleLinear().domain([ 0, sampleData1.length + 1 ]).range([ padding, w - padding ]);
+  const xScale = d3.scaleLinear().domain([ 0, sampleData[index].length + 1 ]).range([ padding, w - padding ]);
 
   // create a y scale the maps the vertical range to the range of values in the array
   // the range looks flipped because SVG puts the origin in the top left, not the bottom left
-  const yScale = d3.scaleLinear().domain([ d3.min(sampleData1) - 1, d3.max(sampleData1) + 1]).range([ h - padding, padding ]);
+  const yScale = d3.scaleLinear().domain([ d3.min(sampleData[index]) - 1, d3.max(sampleData[index]) + 1]).range([ h - padding, padding ]);
 
   const plot_circles = plot.selectAll('circle');    // selector for all circles in the plot
-  const plot_data = plot_circles.data(sampleData1); // bind the latest version of sampleData1
+  const plot_data = plot_circles.data(sampleData[index]); // bind the latest version of sampleData[index]
   
-  const plot_new_data = plot_data.enter() // do the following for every element of sampleData1 that is getting added
+  const plot_new_data = plot_data.enter() // do the following for every element of sampleData[index] that is getting added
     .append('circle')     // append a circle for each element in the data set
     .classed('data_point', true) // assign the data_point class to this element
 
@@ -59,5 +66,3 @@ function updatePlot() {
   d3.select('#xaxis').call(xAxis).attr('transform', `translate(0 ${h - padding})`);
   d3.select('#yaxis').call(yAxis).attr('transform', `translate(${padding} 0)`);
 }
-
-updatePlot();
